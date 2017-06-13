927312c2187a16f10a6217dbba3b5390fe7f99b2
liupan <liupan@foreveross.com>
2017-06-13 14:34:37 +0800

    打包前清空旧文件。

--- build/build.js
+++ build/build.js
@@ -1,20 +1,29 @@
 process.env.NODE_ENV = 'production';
+var rm = require('rimraf');
+var path = require('path');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.prod.conf');
+var config = require('../config/index');
 console.log('Build start...');
-webpack(webpackConfig, function (err, stats) {
+rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), (err) => {
   if (err) throw err;
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n\n');
+  rm(config.build.html, err => {
+    if (err) throw err;
+    webpack(webpackConfig, function (err, stats) {
+      if (err) throw err;
+      process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+      }) + '\n\n');
 
-  console.log('  Build complete.\n');
-  console.log(
-    '  Tip: built files are meant to be served over an HTTP server.\n' +
-    '  Opening index.html over file:// won\'t work.\n'
-  );
+      console.log('  Build complete.\n');
+      console.log(
+        '  Tip: built files are meant to be served over an HTTP server.\n' +
+        '  Opening index.html over file:// won\'t work.\n'
+      );
+    });
+  });
 });
